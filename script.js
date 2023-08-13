65202e6d7a3377159dd167e0325c8672671230a9
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 00:54:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
-    //Write your code here
-
-
+	const boldWords = document.querySelectorAll('strong');
+  for (const word of boldWords) {
+    word.style.color = 'green';
+  }
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+   const boldWords = document.querySelectorAll('strong');
+  for (const word of boldWords) {
+    word.style.color = 'black';
+  }
 }
